5f32e3d164851e53775d7361e34bc9f0ffeea009
Rocio <110997512+RoLarrosa@users.noreply.github.com>
2023-10-25 10:30:24 -0300

    subo ejemplo con Native Base

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Box, Heading, Stack } from "native-base";
+import { Icon, Pressable } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+import {
+  Center,
+  NativeBaseProvider,
+  VStack,
+  Input,
+  Button
+} from "native-base";
+
+const Example = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <Box alignItems="center">
+      <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
+        borderColor: "coolGray.600",
+        backgroundColor: "gray.700"
+      }} _web={{
+        shadow: 2,
+        borderWidth: 0
+      }} _light={{
+        backgroundColor: "gray.50"
+      }}>
+        <Stack p="4" space={3}>
+          <VStack space={1} alignItems="center">
+            <Heading size="lg">
+              Inicio de Sesión
+            </Heading>
+          </VStack>
+          { /* INICIO: FORMULARIO */}
+          <Stack space={4} w="100%" maxW="100%" mx="auto" alignItems="center">
+            <Input
+              w={{
+                //base: "75%",
+                md: "25%"
+              }}
+              InputLeftElement={
+                <Icon
+                  as={<MaterialIcons name="person" />}
+                  size={5}
+                  ml="2"
+                  color="muted.400"
+                />
+              }
+              placeholder="DNI"
+            />
+            <Input
+              w={{
+                //base: "75%",
+                md: "25%"
+              }}
+              type={show ? "text" : "password"}
+              InputRightElement={
+                <Pressable onPress={() => setShow(!show)}>
+                  <Icon as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" />
+                </Pressable>
+              }
+              placeholder="Contraseña"
+            />
+          </Stack>
+          { /* FIN: FORMULARIO */}
+          <Box alignItems="center">
+            <Button onPress={() => console.log("hello world")}>Ingresar</Button>
+          </Box>
+          
+        </Stack>
+      </Box>
+    </Box>
+  )
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center px={4} flex={1}>
+        <VStack space={5} alignItems="center">
+          <Example />
+        </VStack>
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
